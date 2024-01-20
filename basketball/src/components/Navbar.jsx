55c6dd94dc8950d9bdfbd0af9e1bc072af1f4d34
Tichyyy <tijana.shojich.2001@gmail.com>
2024-01-20 14:36:48 +0100

    dodat header, navbar livescore stranice

---
+++ basketball/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../App";
+
+
+const Navbar= () => {
+    return <div className="navbar">
+        <h1>Basketball videos 🏀</h1>
+    </div>;
+};
+
+export default Navbar;
