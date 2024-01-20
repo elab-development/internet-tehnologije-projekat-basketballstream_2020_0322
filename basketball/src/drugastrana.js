b9619f354e24c75f27b6f89cece69957fc9a0f32
Tichyyy <tijana.shojich.2001@gmail.com>
2024-01-20 19:55:05 +0100

    azuriranje

--- basketball/src/drugastrana.js
+++ basketball/src/drugastrana.js
@@ -3,14 +3,15 @@ import React from "react";
 import Footer from "./components/Footer";
 import Navbar2 from "./components2/Navbar2";
 import Content2 from "./components2/Content2";
+import './App2.css';
 
 const DrugaStrana = () => {
   return (
     <div>
     <Navbar2/>
     <Content2/>
-      <h1>This is DrugaStrana.js</h1>
-      {/* Add content for the new page */}
+     
+    
     </div>
   );
 };
