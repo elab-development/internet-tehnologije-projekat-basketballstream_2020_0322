ddbad91acc38b00c870bb5cae0c956a5105b9b2e
Tichyyy <tijana.shojich.2001@gmail.com>
2024-01-20 16:31:49 +0100

    dodate rute kod App za drugu stranu

--- basketball/src/App.js
+++ basketball/src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Content from './components/Content';
+import DrugaStrana from './drugastrana'; // Import the new page
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Content/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Content />} />
+          <Route path="/drugastrana" element={<DrugaStrana />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
-
-
 export default App;
+
