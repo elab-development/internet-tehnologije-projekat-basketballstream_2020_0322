ea72871a6a90243ad9c31983a2dee71e142fd082
Tichyyy <tijana.shojich.2001@gmail.com>
2024-01-20 16:30:58 +0100

    dodata druga strana

---
+++ basketball/src/drugastrana.js
@@ -0,0 +1,13 @@
+// DrugaStrana.js
+import React from "react";
+
+const DrugaStrana = () => {
+  return (
+    <div>
+      <h1>This is DrugaStrana.js</h1>
+      {/* Add content for the new page */}
+    </div>
+  );
+};
+
+export default DrugaStrana;
