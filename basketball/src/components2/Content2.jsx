997c0ff105c11595f3bca4cfb96a65ab5bd2f5e4
Tichyyy <tijana.shojich.2001@gmail.com>
2024-01-20 16:48:29 +0100

    content, navbar za drugu stranicu dodati

---
+++ basketball/src/components2/Content2.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import "../drugastrana";
+
+const Content2 = () => {
+  const [teams, setTeams] = useState([]);
+  const apiUrl = 'https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setTeams(data.sports[0].leagues[0].teams);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [apiUrl]);
+
+  return (
+    <div className="content-container">
+      <h1>NBA Teams</h1>
+      <ul className="team-list">
+        {teams.map((team) => (
+          <li key={team.team.id} className="team-card">
+            <img src={team.team.logos[0].href} alt={team.team.displayName} />
+            <h2>{team.team.displayName}</h2>
+            <p>{team.team.location}</p>
+            {/* Add more information or links as needed */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Content2;
---
+++ basketball/src/components2/Navbar2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../drugastrana";
+
+
+const Navbar2= () => {
+    return <div className="navbar">
+        <h1>Basketball videosss 🏀</h1>
+    </div>;
+};
+
+export default Navbar2;
