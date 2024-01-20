f90bc5eb33b9a63910e48880491a8eeba083f327
Tichyyy <tijana.shojich.2001@gmail.com>
2024-01-20 14:32:12 +0100

    footer live scoreboard stranice

---
+++ basketball/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../App";
+
+
+const Footer= () => {
+    return <div className="footer">
+        <p>Basketball Videos App 2024&copy;</p>
+        </div>;
+};
+
+export default Footer;
