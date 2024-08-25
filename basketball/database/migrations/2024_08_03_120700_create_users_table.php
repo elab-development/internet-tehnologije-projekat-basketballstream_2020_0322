73412d51a9aab5310c85c19d3aa629bf7a19633e
Tichyyy <145337855+Tichyyy@users.noreply.github.com>
2024-08-25 13:54:05 +0200

    database file
    including migrations, factories information

---
+++ basketball/database/migrations/2024_08_03_120700_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('points');
+            $table->string('asists');
+            $table->string('rebounds');
+            $table->string('three_pt');
+            $table->string('two_pt');
+            $table->string('team');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
